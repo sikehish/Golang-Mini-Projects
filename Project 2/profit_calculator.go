c19dfecde71cf63e6d4642e33a4846aca369b846
sikehish <hisham0502@gmail.com>
2024-02-02 01:13:54 +0530

    MInor mods made to Project 2

--- Project 2/profit_calculator.go
+++ Project 2/profit_calculator.go
@@ -24,8 +24,14 @@ func main() {
 
 	//NOTE: %f prints the corresponding number as a decimal floating point number (e.g. 321.65), %e prints the number in scientific notation (e.g. 3.2165e+2). Generally speaking, %g strips any trailing zeroes and trailing decimal point from these two representations and prints whichever is shortest.
 
-	fmt.Printf("EBT: %v\n", ebt) //Prints value in its default format
-	fmt.Printf("Profit: %g\n", profit)
-	fmt.Printf("Ratio: %f\n", ratio)
+	// fmt.Printf("EBT: %v\n", ebt) //Prints value in its default format
+	// fmt.Printf("Profit: %g\n", profit)
+	// fmt.Printf("Ratio: %f\n", ratio)
+	//OR
+	//Formatted Strings approach
+	formattedEBT := fmt.Sprintf("EBT: %v\n", ebt)
+	formattedRFV := fmt.Sprintf("Profit: %g\nRatio: %f\n", profit, ratio) //Combining multiple strings
+
+	fmt.Print(formattedEBT, formattedRFV)
 
 }
