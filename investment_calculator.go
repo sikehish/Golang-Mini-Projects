754f1d62411314a6a2fc44d5ed0e68811928bc8d
sikehish <hisham0502@gmail.com>
2024-02-01 23:22:21 +0530

    Future value adjusted for inflation

--- investment_calculator.go
+++ investment_calculator.go
@@ -13,12 +13,28 @@ func main() {
 	// var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
 	// fmt.Println(futureValue)
 
-	//without type conversions by explicit declaration of types
+	//without type conversions by explicit declaration of type
+	// var investmentAmount float64 = 1000
+	// var years float64 = 10
+	//OR
+
+	// var investmentAmount, years float64 = 1000, 10 //WORKS ONLY FIF TYPES ARE SAME
+	//OR
+	// investmentAmount, years := 1000.0, 10.0
+	// expectedReturnRate := 5.5 //OR var expectedReturnRate = 5.5
+	//OR
+	// investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	// OR
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	years := 10.0
+	expectedReturnRate := 5.5
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	//Factoring in inflation:
+	const inflationRate = 2.5 //OR const inflationRate float64=2.5
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
-
+	fmt.Println(futureRealValue)
 }
