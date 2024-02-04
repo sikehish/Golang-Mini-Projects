2a5fd2031e2ff75d4ed5544a0d10874627a172af
sikehish <hisham0502@gmail.com>
2024-02-05 02:12:24 +0530

    Project 5 underway

---
+++ Project 5/profit_calc_v3.go
@@ -0,0 +1,35 @@
+//Project 5: Enhanced profit calculator
+
+//Goals:
+//1. Validate user input: Show error messages and exit if invalid input is provided, Nonegative numbers and 0s as inputs
+//2. Store calculated results into a file
+
+package main
+
+import "fmt"
+
+func main() {
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
