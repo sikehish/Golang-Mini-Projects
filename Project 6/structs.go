8db033bb33ddeaf00f67f4836f813bc58110aa72
sikehish <hisham0502@gmail.com>
2024-02-05 23:40:31 +0530

    Porject 6 setup

---
+++ Project 6/structs.go
@@ -0,0 +1,24 @@
+// PROJECT 6: Project revolving around structs
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	// ... do something awesome with that gathered data!
+
+	fmt.Println(firstName, lastName, birthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
