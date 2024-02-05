04ad3b4c8001de214130b5bdd810ee141e4c19db
sikehish <hisham0502@gmail.com>
2024-02-06 00:26:58 +0530

    Included method on the struct

--- Project 6/structs.go
+++ Project 6/structs.go
@@ -7,6 +7,10 @@ import (
 	"time"
 )
 
+//VVI NOTE: FUNCTION VS METHODS
+//FUNCTION(NOT METHOD): Accept a struct and operates on the struct
+//METHOD: Uses a receiver(which is the struct on which the method has to be called)-->methods are defined on structs
+
 type user struct {
 	firstName string
 	lastName  string
@@ -14,14 +18,81 @@ type user struct {
 	createdAt time.Time
 }
 
+//Method
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	// ... do something awesome with that gathered data!
+	//Instantiating user struct
 
-	fmt.Println(firstName, lastName, birthdate)
+	// //Approach 1(You can also optionally omiy fields, which would then be set to null)
+	// var appUser user
+	// appUser = user{
+	// 	firstName: userFirstName,
+	// 	lastName:  userLastName,
+	// 	birthdate: userBirthdate,
+	// 	createdAt: time.Now(),
+	// }
+	// OR
+	// appUser = user{
+	// 	userFirstName,
+	// 	userLastName,
+	// 	userBirthdate,
+	// 	time.Now(),
+	// }
+
+	// //Approach 2
+	// var appUser user = user{
+	// 	firstName: userFirstName,
+	// 	lastName:  userLastName,
+	// 	birthdate: userBirthdate,
+	// 	createdAt: time.Now(),
+	// }
+
+	// // Approach 3
+	// var appUser = user{
+	// 	firstName: userFirstName,
+	// 	lastName:  userLastName,
+	// 	birthdate: userBirthdate,
+	// 	createdAt: time.Now(),
+	// }
+
+	//Approach 4
+	appUser := user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	//NOTE: There is another approach(using a function call like new to instantiate a struct)
+
+	// outputUserDetails(userFirstName, userLastName, userBirthdate)
+	// outputUserDetails(appUser)
+	// outputUserDetails(&appUser) //Pointer based approach
+	//OR Method based approach
+	appUser.outputUserDetails()
+
+}
+
+//FUNCTIONS
+// func outputUserDetails(firstName, lastName, birthdate string) {
+// 	fmt.Println(firstName, lastName, birthdate)
+// }
+// Pass by value approach below
+// func outputUserDetails(u user) {
+// 	fmt.Println(u.firstName, u.lastName, u.birthdate)
+// }
+
+//Pass by ref approach below:
+func outputUserDetails(u *user) {
+	//Pointers to struct exception: Normally you'd dereference to access the value like: (*u).birtthdate, which is valid, but in go, we have a shortcut/exception: (*u).birthdate is same as u.birthdate(As you can see in the below line)
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
 }
 
 func getUserData(promptText string) string {
