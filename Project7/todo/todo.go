2b466b673b9b79a4670f02f6656b5d5250f0cab8
sikehish <hisham0502@gmail.com>
2024-02-07 17:12:10 +0530

    Project7: Setup todo package

---
+++ Project7/todo/todo.go
@@ -0,0 +1,44 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+// Struct
+type Todo struct {
+	Text string `json:"content"`
+}
+
+//Struct tags are metadata that can be added to the struct fields(we use it here as the json package(encoding/json) can then come up with custom key in the JSON data corresponding to each struct field based on the tags that we provide in the aboce struct)
+
+// Constructor
+func New(content string) (Todo, error) {
+	if content == "" {
+		return Todo{}, errors.New("Invalid input")
+	}
+
+	newNote := Todo{
+		Text: content,
+	}
+
+	return newNote, nil
+}
+
+// Methods
+func (todo Todo) Display() {
+	fmt.Println(todo.Text)
+}
+
+func (todo Todo) Save() error {
+	fileName := "todo.json"
+	jsonData, err := json.Marshal(todo) //for the note's fields to be accessible, the fields need to start with capital letters
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, jsonData, 0644) //it returns an error, so we're returning it. If it returns the error returned will be nil
+}
