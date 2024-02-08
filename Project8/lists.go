ac6da22c33bc248a8782f3a2e95710d7abdab681
sikehish <hisham0502@gmail.com>
2024-02-08 13:58:32 +0530

    added content related to arrays and slicing arrays

--- Project8/lists.go
+++ Project8/lists.go
@@ -1,1 +1,39 @@
-//PROJECT 8: Learning about arrays,slices and maps
+// PROJECT 8: Learning about arrays,slices and maps
+package main
+
+import "fmt"
+
+func main() {
+	//Arrays
+	// var productName [4]string //4 empty slots/strings
+	// productName = [4]string{"A", "B", "C", "D"}
+	//OR
+	var products [4]string = [4]string{"A", "B", "C", "D"} //OR
+	var productName [4]string = [4]string{"A Book"}        //OR Initialising the array with only 1 value, and the remaining slots are initialised with empty string
+	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	fmt.Println(prices)
+	fmt.Println(products)
+	fmt.Println(productName)
+
+	//Creating a slice from the array
+	// featuredPrices := prices[1:3] //Includes elements starting from index 1 to index 3-1=2.
+	// featuredPrices := prices[:3]   //From index 0 to 2
+	featuredPrices := prices[1:] //From index 1 to las idx=3
+	//NOTE: negative indices and indices larger than the array size arent allowed
+	fmt.Println(featuredPrices, len(featuredPrices), cap(featuredPrices))
+
+	//Creating a slice from a slice:
+	highlightedPrices := featuredPrices[:1]
+	fmt.Println(highlightedPrices, len(highlightedPrices), cap(highlightedPrices))
+	//In the above example, len=1 and capacity=3. This is because the slice from which it is sliced has 3 elements, and here were obtaining just the first element. We can also extend highlightedPrices towards the right and can span across all 3 elements instead of just 1. Note that if highlighted prices would sotre the slice from 1:2(the featured Prices has 3 elements), then we would only get access to the 1st element, with an option to extend/span across index 2 as well starting from index 1, but we cannot span index 0 element as it is towards the left of the starting index. Below are a few exampels:
+
+	//As capacity of highlightedPrices in the above example is 3,we can do the following
+	highlightedPrices = highlightedPrices[0:] //we obtin just the first element, but if we explictly mention the ending index, and if its less than or equal to capacity, then we shall see other elements stored in the base slice
+	highlightedPrices = highlightedPrices[0:3]
+	fmt.Println(featuredPrices, highlightedPrices, len(highlightedPrices), cap(highlightedPrices))
+
+	highlightedPrices = featuredPrices[1:2]
+	fmt.Println(featuredPrices, highlightedPrices, len(highlightedPrices), cap(highlightedPrices))
+	//Now, highlightedPrices:=highlightedPrices[0:2] will fetch index 1 an index 2 elements of the featured prices, and there's no way that we'd be able to access any elements to the left of index1 as slice starts from index 1
+
+}
--- Project8/practice.go
+++ Project8/practice.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+//Tasks to be accomplished are mentioned after the main function
 
 //Solution: https://github.com/mschwarzmueller/go-complete-guide-resources/blob/main/code/07-arrays-slices-maps/07-exercise-solution/practice.go
 
@@ -11,51 +11,53 @@ type Product struct {
 	price float64
 }
 
-func main() {
+// // Commenting the below main func code as i have another main fucn in the same module in lists.go. Uncomment whenever you want to execute
+// func main() {
 
-	//Task 1
-	hobbies := [3]string{"Coding", "Photography", "Football"} //OR var hobbies = [3]string{"Coding", "Photography", "Football"} OR var hobbies [3]string = [3]string{"Coding", "Photography", "Football"}
-	fmt.Println("My Hobbies: ", hobbies)
-	fmt.Println("----------------------------------------------------------")
+// 	//Task 1
+// 	hobbies := [3]string{"Coding", "Photography", "Football"} //OR var hobbies = [3]string{"Coding", "Photography", "Football"} OR var hobbies [3]string = [3]string{"Coding", "Photography", "Football"}
+// 	fmt.Println("My Hobbies: ", hobbies)
+// 	fmt.Println("----------------------------------------------------------")
 
-	//Task 2
-	ele := hobbies[0:1] //OR hobbies[:1]//OR, if you dont want a slice, and only want the element, ele := hobbies[0]
-	arr := hobbies[1:]  //OR arr:=hobbies[1:3]
-	fmt.Printf("%v\n%v\n", ele, arr)
-	fmt.Println("----------------------------------------------------------")
+// 	//Task 2
+// 	ele := hobbies[0:1] //OR hobbies[:1]//OR, if you dont want a slice, and only want the element, ele := hobbies[0]
+// 	arr := hobbies[1:]  //OR arr:=hobbies[1:3]
+// 	fmt.Printf("%v\n%v\n", ele, arr)
+// 	fmt.Println("----------------------------------------------------------")
 
-	//Task 3
-	slc1 := ele[:2]
-	slc2 := ele[0:2]
-	fmt.Println(slc1, slc2)
-	fmt.Println("----------------------------------------------------------")
+// 	//Task 3
+// 	slc1 := ele[:2]
+// 	slc2 := ele[0:2]
+// 	fmt.Println(slc1, slc2)
+// 	fmt.Println("----------------------------------------------------------")
 
-	//Task 4
-	slc := slc1[1:3]
-	fmt.Println(slc)
-	fmt.Println("----------------------------------------------------------")
+// 	//Task 4
+// 	slc := slc1[1:3]
+// 	fmt.Println(slc)
+// 	fmt.Println("----------------------------------------------------------")
 
-	//Task 5
-	courseGoals := []string{"Get a good score", "Get a good job"}
-	fmt.Println(courseGoals)
-	fmt.Println("----------------------------------------------------------")
+// 	//Task 5
+// 	courseGoals := []string{"Get a good score", "Get a good job"}
+// 	fmt.Println(courseGoals)
+// 	fmt.Println("----------------------------------------------------------")
 
-	//Task 6
-	courseGoals[1] = "Get a high paying job"
-	courseGoals = append(courseGoals, "Make good friends") //Overwrites the exisitng slice
-	// courseGoals2 := append(courseGoals, "Make good friends") //OR we can store the result in a different slice, as append doesnt modify the exisiting slice, insted it returns a new one
-	fmt.Println(courseGoals)
-	fmt.Println("----------------------------------------------------------")
+// 	//Task 6
+// 	courseGoals[1] = "Get a high paying job"
+// 	courseGoals = append(courseGoals, "Make good friends") //Overwrites the exisitng slice
+// 	// courseGoals2 := append(courseGoals, "Make good friends") //OR we can store the result in a different slice, as append doesnt modify the exisiting slice, insted it returns a new one
+// 	fmt.Println(courseGoals)
+// 	fmt.Println("----------------------------------------------------------")
 
-	//Task 7(Bonus)
-	productList := []Product{Product{"Harpic", "ABC123", 180.0}, Product{"Dettol", "ABC256", 185.5}}
-	// OR productList := []Product{{"Harpic", "ABC123", 180.0}, {"Dettol", "ABC256", 185.5}}
-	fmt.Println(productList)
-	productList = append(productList, Product{"Garlic", "DEF234", 25.74})
-	fmt.Println(productList)
-	fmt.Println("----------------------------------------------------------")
-}
+// 	//Task 7(Bonus)
+// 	productList := []Product{Product{"Harpic", "ABC123", 180.0}, Product{"Dettol", "ABC256", 185.5}}
+// 	// OR productList := []Product{{"Harpic", "ABC123", 180.0}, {"Dettol", "ABC256", 185.5}}
+// 	fmt.Println(productList)
+// 	productList = append(productList, Product{"Garlic", "DEF234", 25.74})
+// 	fmt.Println(productList)
+// 	fmt.Println("----------------------------------------------------------")
+// }
 
+// -------------------------TASKS-------------------------------------
 // Time to practice what you learned!
 
 // 1) Create a new array (!) that contains three hobbies you have
