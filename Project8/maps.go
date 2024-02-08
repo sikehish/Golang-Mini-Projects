47efd24192df1ec2e4a19b5f75f4653759a118e0
sikehish <hisham0502@gmail.com>
2024-02-08 16:14:16 +0530

    Added maps related content with examples

--- Project8/maps.go
+++ Project8/maps.go
@@ -1,8 +1,35 @@
 // PROJECT 8: Learning about arrays,slices and maps
-//MAPS: A key value based Data structure
+//MAPS: A key value based Data structure. They are dynamic DS
 
 package main
 
+import "fmt"
+
 func main() {
+	//  websites:=map[string]string{} //-->map declaration
+	// //OR
+	// var websites map[string]string = map[string]string{}
+	websites := map[string]string{
+		"Google":              "https://google.com",
+		"Amazon Web Services": "https://aws.com",
+	}
+	fmt.Println(websites)
+
+	//Accessing values
+	fmt.Println(websites["Linkedin"] == "") //Basically it doesnt exist, hence empty string ouput
+	fmt.Println(websites["Amazon Web Services"])
+
+	//Adding a new key value pair
+	websites["Linkedin"] = "https://linkedin.com"
+	fmt.Println("After inserting linkedin: ", websites)
+
+	//Deleting a key value pair
+	delete(websites, "Google")
+	fmt.Println("After deleting google: ", websites)
+
+	//Maps vs structs: 2 major differences
+	//1.A key in map can be of any data type, while in struct, its always a string
+	//2. With structs, we have a predefined  DS, and we cant really delete/add a new key. Structs solve a different problem: to a have a collection of items, while maps store akey value pairs
+	//Struct is more descriptive than maps
 
 }
