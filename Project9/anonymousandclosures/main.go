2fd2312232b9cb177b3072ba5543d076fc466951
sikehish <hisham0502@gmail.com>
2024-02-09 18:19:30 +0530

    Project9: Closures covered

--- Project9/anonymousandclosures/main.go
+++ Project9/anonymousandclosures/main.go
@@ -10,6 +10,14 @@ func main() {
 		return number * 2
 	})
 	fmt.Println(transformed)
+
+	//Closures
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
+	doubledArr := transformNumbers(&numbers, double)
+	tripledArr := transformNumbers(&numbers, triple)
+	fmt.Println(doubledArr, tripledArr)
 }
 
 func transformNumbers(numbers *[]int, transform func(int) int) []int {
@@ -25,7 +33,7 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 // Closures
 // createTransformer is a factory function.In Go, a factory function is a function that returns a new instance of a type. The purpose of a factory function is to encapsulate the process of creating an object, providing flexibility and abstraction for the user.For example, the New() function that we used with structs are factory funcs
 // Functions are closures in go.closure is a function value that references variables from its containing function's body. In Go, if you define a function inside another function and it references variables from the outer function, it becomes a closure.
-func createTransformer() func(int) int {
+func createTransformer(factor int) func(int) int {
 	return func(number int) int {
 		return number * factor
 	}
