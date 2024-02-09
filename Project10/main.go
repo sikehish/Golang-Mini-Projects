cc56872330c16a582c410df2b7820857bff5d238
sikehish <hisham0502@gmail.com>
2024-02-10 00:57:41 +0530

    Goroutines code underway

--- Project10/main.go
+++ Project10/main.go
@@ -2,6 +2,29 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func greet(phrase string) {
+	fmt.Println("Hello!", phrase)
+}
+
+func slowGreet(phrase string) {
+	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
+	fmt.Println("Hello!", phrase)
+}
+
+func main() {
+	// greet("Nice to meet you!")
+	// greet("How are you?")
+	// slowGreet("How ... are ... you ...?")
+	// greet("I hope you're liking the course!")
+
+	//Converting the above functions to goroutines
+	go greet("Nice to meet you!")
+	go greet("How are you?")
+	go slowGreet("How ... are ... you ...?")
+	go greet("I hope you're liking the course!")
 }
